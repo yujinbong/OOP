299a0a42cc88784aeffc295d356a2ae2d54ec9ad
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-04-21 10:05:17 -0400

    Create Car.h

---
+++ OOP244/W10/Car.h
@@ -0,0 +1,33 @@
+/*------------------------------------------------------
+Workshop 10 part 1
+ name: Yujin Bong
+SECTION : NCC
+PROFESSOR : Arezoo.tony
+Module: Car
+Version 1.0
+Author : Fardad Soleimanloo  22/04/01
+Revision History
+   I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+-----------------------------------------------------------
+Date       Reason
+----------------------------------------------------------- */
+#ifndef SDDS_CAR_H_
+#define SDDS_CAR_H_
+#include "Displayable.h"
+namespace sdds{
+    class Car :public Displayable{
+        char m_plate[9]{};
+        char m_make[41]{};
+        char m_model[41]{};
+        void set(const char* plate, const char* make, const char* model);
+    public:
+        Car();
+        Car(const char* plate, const char* make, const char* model );
+        std::ostream& display(std::ostream& os)const;
+        bool operator==(const Car& C)const;
+    };
+}
+
+
+#endif
