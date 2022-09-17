b54ba24f9e8b40a47d45c89182d4379e335b5978
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-09-17 13:25:45 -0400

    Update Collection.h

--- OOP345/W3/W3_P2/Collection.h
+++ OOP345/W3/W3_P2/Collection.h
@@ -1,49 +1,78 @@
-#ifndef SDDS_COLLECTION_H
+// Workshop 3 - Templates
+// 2022/01/28 - If this code works, it was created by Cornel
+//            - If it doesn't work, I don't know who wrote it.
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+
+#ifndef  SDDS_COLLECTION_H
 #define SDDS_COLLECTION_H
+#include <iomanip>
+#include <iostream>
 #include "Pair.h"
+
 namespace sdds {
-	template<typename T, size_t CAPACITY>
-	class Collection
-	{
-		T m_array[CAPACITY];
-		size_t m_arraySize{};
-		static T dummy;
+
+	template<typename T, size_t capacity>
+	class Collection  {
+		T m_array[capacity]{};
+		size_t m_noOfElement{};
+		static T dummy;  //to save memory
 	public:
-		size_t size()const {
-			return m_arraySize;
+		//part2
+		~Collection() {}
+		size_t size() const {
+			return m_noOfElement;
 		}
-		virtual ~Collection() {}
-		void display(std::ostream& os = std::cout) const {
-			os << "----------------------" << std::endl;
+		void display(std::ostream& os = std::cout)const { 
+			/*
+			----------------------
+			| Collection Content |
+			----------------------
+			16
+			12
+			9
+			3
+			21
+			12
+			23
+			----------------------
+	*/
+			os <<  std::setfill('-') << std::setw(22) <<"-"<<std::endl;
 			os << "| Collection Content |" << std::endl;
-			os << "----------------------" << std::endl;
-			for (size_t i = 0; i < m_arraySize; i++) {
+			os << std::setfill('-') << std::setw(22) << "-" << std::endl;
+			for (size_t i = 0; i < m_noOfElement; i++) { 
 				os << m_array[i] << std::endl;
 			}
-			os << "----------------------" << std::endl;
+			os << std::setfill('-') << std::setw(22) << "-" << std::endl;
 		}
-		virtual bool add(const T& item) {
-			bool result = false;
-			if (m_arraySize < CAPACITY) {
-				m_array[m_arraySize] = item;
-				m_arraySize++;
-				result = true;
-			}
-			return result;
-		}
-		T operator[](size_t index) const {
-			return (index >= 0 && index < m_arraySize) ? m_array[index] : dummy;
-		}
-		
-	};
 
-	template<class T, size_t CAPACITY>
-	T Collection<T, CAPACITY>::dummy{};
+		virtual bool add(const T& item) {
+			bool flag = false;
+		
+			if (m_noOfElement < capacity) {
+				m_array[m_noOfElement] = item;
+				m_noOfElement++;
+				flag =true;
+		}
+			return flag; 
+		}
+	
+		T operator[](size_t index) const {
+			return index >= 0 && index < m_noOfElement ? m_array[index] : dummy;
+		}
+
+	
+
+
+		};
+
+    template <typename T,size_t capacity>
+	T Collection<T, capacity>::dummy{};
 
 	template<>
 	Pair Collection<Pair, 100>::dummy{ "No Key", "No Value" };
+
 }
 
 
-#endif
-
+#endif // ! COLLECTION_H
