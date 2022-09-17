0d2d39549380a6c7bc82a4ec2c632280ce95e10d
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-09-17 13:31:48 -0400

    Update Child.h

--- OOP345/W4/W4_P2/Child.h
+++ OOP345/W4/W4_P2/Child.h
@@ -1,3 +1,11 @@
+// w4_p2.cpp - Containers
+// 2021/12/02 - Miguel
+// 2022/02/02 - Cornel
+// Professor    : Chris Szalwinski
+// Student      : Yujin Bong
+// Student ID   : 147-525-208
+// SECTIOMN     : NEE 
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #ifndef  SDDS_CHILD_H
 #define  SDDS_CHILD_H
 #include <iostream>
@@ -10,8 +18,8 @@ namespace sdds {
 		const sdds::Toy** m_toyArray{};
 		size_t m_arraySize{};
 
+
 	public:
-		
 		Child(std::string name, int age, const Toy* toys[], size_t count);
 		Child(const Child& I);
 		Child(Child&& I) noexcept;
