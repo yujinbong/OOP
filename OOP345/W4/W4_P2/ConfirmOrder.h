4a31e317c15ecd32bc0a032888fd2f85cea903ee
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-09-17 13:30:49 -0400

    Update ConfirmOrder.h

--- OOP345/W4/W4_P2/ConfirmOrder.h
+++ OOP345/W4/W4_P2/ConfirmOrder.h
@@ -1,3 +1,11 @@
+// w4_p2.cpp - Containers
+// 2021/12/02 - Miguel
+// 2022/02/02 - Cornel
+// Professor    : Chris Szalwinski
+// Student      : Yujin Bong
+// Student ID   : 147-525-208
+// SECTIOMN     : NEE 
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #ifndef  SDDS_CONFIRMORDER_H
 #define   SDDS_CONFIRMORDER_H
 #include <iostream>
