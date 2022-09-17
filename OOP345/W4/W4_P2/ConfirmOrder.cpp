9d16c8eaba52c6d1c2f5435d3dd8c588ba08c87a
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-09-17 13:32:10 -0400

    Update ConfirmOrder.cpp

--- OOP345/W4/W4_P2/ConfirmOrder.cpp
+++ OOP345/W4/W4_P2/ConfirmOrder.cpp
@@ -1,3 +1,11 @@
+// w4_p2.cpp - Containers
+// 2021/12/02 - Miguel
+// 2022/02/02 - Cornel
+// Professor    : Chris Szalwinski
+// Student      : Yujin Bong
+// Student ID   : 147-525-208
+// SECTIOMN     : NEE 
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 #include "ConfirmOrder.h"
 
 
@@ -26,21 +34,20 @@ namespace sdds {
 			{
 				m_toyArray[i] = other.m_toyArray[i];
 			}
-		
 		}
 		return *this;
 	}
+
 	ConfirmOrder& ConfirmOrder::operator=(ConfirmOrder&& other) noexcept
 	{
 		if (this != &other) {
 			delete[] m_toyArray;
 			m_noOfChild = other.m_noOfChild;
 			m_toyArray = other.m_toyArray;
-			other.m_noOfChild = {};
 			other.m_toyArray = nullptr;
-			
+			other.m_noOfChild = 0;
 		}
-		return* this;
+		return *this;
 	}
 
 	ConfirmOrder::~ConfirmOrder()
@@ -50,41 +57,62 @@ namespace sdds {
 
 	ConfirmOrder& ConfirmOrder::operator+=(const Toy& toy)
 	{
-		//현재 컨펌오더의 어레이에 적어도 하나의 토이가 있는지 확인
-		bool isFound = false;
-		if (m_noOfChild > 0) { 
-			for (size_t i = 0; i < m_noOfChild; i++)
-			{
-		       //m_toyArray[i] i는(자료형) toy*(토이의 포인터)
-				if (m_toyArray[i] == &toy)//m_toyArray[i]가 toy값과 같은지 확인하고 같으면 bool값 true로 변경 후 for문 빠져나옴
-					isFound = true;
-			}
-			if (!isFound)
-			{
-				const Toy** temp = m_toyArray; //temp라는 Toy**의 임시값을 만들어서 하나씩 추가한다.
-				m_toyArray = new const Toy * [m_noOfChild+1];
-				for (size_t i = 0; i < m_noOfChild; i++)
-				{
-					m_toyArray[i] = temp[i];
-				}
-				m_toyArray[m_noOfChild++] = &toy;
-				delete[] temp;
-			}
-
-		}
-		else {
+		bool flag = false;
+		if (m_noOfChild == 0) {
 			m_toyArray = new const Toy * [1];
+			//m_toyArray[m_noOfChild] = &toy;
 			m_toyArray[0] = &toy;
 			m_noOfChild++;
 		}
-		
+		else if (m_noOfChild > 0) {
+			for (size_t i = 0; i < m_noOfChild && !flag; i++)
+			{
+				if (m_toyArray[i] == &toy) flag = true;
+			}
+			if (!flag) {
+				//ConfirmOrder temp = *this;
+				const Toy** temp = m_toyArray;
+				m_toyArray = new const Toy * [++m_noOfChild];
+				for (size_t i = 0; i < m_noOfChild -1; i++)
+				{
+					m_toyArray[i] = temp[i];
+				}
+				delete[] temp;
+				m_toyArray[m_noOfChild - 1] = &toy;
+			}
+		}
+		return *this;
 	}
 
 	ConfirmOrder& ConfirmOrder::operator-=(const Toy& toy)
 	{
-		bool isFound = false;
-		
+		int flag = -1;
+		if (m_noOfChild > 0) {
+			for (int i = 0; i < (int)m_noOfChild && flag < 0; i++)
+			{
+				if (m_toyArray[i] == &toy) flag = i;
+			}
+			if (flag >= 0) {
+				//ConfirmOrder temp = *this;
+				const Toy** temp = m_toyArray;
+				//delete[] m_toyArray;
+				m_toyArray = new const Toy * [--m_noOfChild];
+				for (int i = 0; i < flag; i++)
+				{
+					//m_toyArray[i] = temp.m_toyArray[i];
+					m_toyArray[i] = temp[i];
+				}
+				for (int i = flag; i < (int)m_noOfChild; i++)
+				{
+					m_toyArray[i] = temp[i + 1];
+				}
+				delete[] temp;
+			}
+		}
+		return *this;
 	}
+
+
 
 	std::ostream& operator<<(std::ostream& os, const ConfirmOrder& c)
 	{
