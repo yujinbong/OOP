d4a858add26ef0f3162086bbaae3a9949e27d1e3
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-09-18 14:36:00 -0400

    Update Autoshop.h

---
+++ OOP345/W6/w6/Autoshop.h
@@ -0,0 +1,26 @@
+#ifndef SDDS_AUTOSHOP_H
+#define SDDS_AUTOSHOP_H
+#include <vector>
+#include <list>
+#include "Vehicle.h"
+namespace sdds {
+	class Autoshop {
+		std::vector<Vehicle*> m_vehicles;
+	public:
+		Autoshop& operator +=(Vehicle* theVehicle);
+		Autoshop();
+		Autoshop(const Autoshop& I) = delete;
+		Autoshop& operator=(const Autoshop& other) = delete;
+		void display(std::ostream& out) const;
+		~Autoshop();
+
+		template<typename T>
+		void select(T test, std::list<const Vehicle*>& vehicles) {
+			for (auto& v : m_vehicles) {
+				if (test(v)) vehicles.push_back(v);
+			}
+		}
+	};
+}
+
+#endif // !SDDS_AUTOSHOP_H
