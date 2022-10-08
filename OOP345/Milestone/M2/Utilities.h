a6c6107357eb540e6c3315c751308ddc8b784c06
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-10-08 14:37:27 -0400

    Create Utilities.h

---
+++ OOP345/Milestone/M2/Utilities.h
@@ -0,0 +1,29 @@
+// Final Project
+// Milestone 2
+// Chris Szalwinski
+// Student Name: Yujin Bong
+// Seneca Student ID: 147-525-208
+// Seneca email:ybong@myseneca.ca
+// Date of completion: July.27
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+#include <string>
+namespace sdds {
+
+	class Utilities {
+		size_t m_widthField = 1;
+		static char m_delimiter;// separates the tokens
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+	};
+
+}
+
+
+#endif
