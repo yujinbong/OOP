e194fed15a17cad768fb29514f77802017e2293a
Yujin <84473069+yujinbong@users.noreply.github.com>
2022-10-08 14:44:32 -0400

    Create Workstation.h

---
+++ OOP345/Milestone/M3/Workstation.h
@@ -0,0 +1,29 @@
+#ifndef SDDS_WORKSTATION_H
+#define SDDS_WORKSTATION_H
+#include <iostream>
+#include <deque>
+#include "CustomerOrder.h"
+#include "Station.h"
+namespace sdds {
+	extern std::deque<CustomerOrder> g_pending;
+	extern std::deque<CustomerOrder> g_completed;
+	extern std::deque<CustomerOrder> g_incomplete;
+
+	class Workstation : public Station {
+		std::deque<CustomerOrder> m_orders;
+		Workstation* m_pNextStation = nullptr;
+	public:
+		Workstation(const std::string& str);
+		void fill(std::ostream& os);
+		bool attemptToMoveOrder();
+		void setNextStation(Workstation* station);
+		Workstation* getNextStation() const;
+		void display(std::ostream& os) const;
+		Workstation& operator+=(CustomerOrder&& newOrder);
+	};
+
+
+}
+
+
+#endif
